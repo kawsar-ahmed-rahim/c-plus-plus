4cda9f2c887206785dbc81698406c59c092216e2
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-09-07 20:36:06 +0600

    till now

---
+++ string/remove_all_occurrences.cpp
@@ -0,0 +1,9 @@
+//! strfind();
+//! str.erase(start,end)
+
+string removeOccurences(string s, string part){
+    while(s.length() > 0 && s.find(part)< s.length()){
+        s.erase(s.find(part), part.length())
+    }
+    return s;
+}
