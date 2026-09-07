c84ec32892b9aaf211228cf06b69a189c437cf14
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-09-07 20:37:54 +0600

    till now

--- string/remove_all_occurrences.cpp
+++ string/remove_all_occurrences.cpp
@@ -1,9 +1,31 @@
 //! strfind();
 //! str.erase(start,end)
 
-string removeOccurences(string s, string part){
-    while(s.length() > 0 && s.find(part)< s.length()){
-        s.erase(s.find(part), part.length())
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string removeOccurrences(string s, string part) {
+
+    while (s.length() > 0 && s.find(part) < s.length()) {
+
+        s.erase(s.find(part), part.length());
     }
+
     return s;
+}
+
+int main() {
+
+    string s, part;
+
+    cin >> s;
+    cin >> part;
+
+    string result = removeOccurrences(s, part);
+
+    cout << result << endl;
+
+    return 0;
 }
