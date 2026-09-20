02e086efd2414928da71c60e4ea5afee77c95b55
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-09-20 19:50:21 +0600

    prime number

--- math/prime_numbers.cpp
+++ math/prime_numbers.cpp
@@ -10,3 +10,19 @@ string isPime(int n) {
 }
 
 // prime number in a range
+
+int countPrime(int n){
+    vector<bool> isPrime(n+1, true);
+    int count = 0;
+
+    fo(int i=2; i<n;i++){
+        if(isPrime[i]){
+            count++;
+
+            for(int j=i*2; j<n; j = j+i){
+                isPrime[j] = false;
+            }
+        }
+    }
+    return count;
+}
