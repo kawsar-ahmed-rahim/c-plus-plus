ad11373e448b53ed3f5ffd9069d7a35e11d2cc40
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-09-20 23:30:03 +0600

    armstrong

--- math/armstrong.cpp
+++ math/armstrong.cpp
@@ -1,2 +1,16 @@
 //! armstrong number
 //armstrong number is equal to the sum of cubes of its digits
+
+bool isArmstrong(int n){
+    int copyN = n;
+    int sumOfCubes = 0;
+
+    while(n!=0){
+        int dig = n % 10;
+        sumOfCubes += (dig*dig*dig);
+
+        n = n / 10;
+    }
+
+    return sumOfCubes == copyN;
+}
