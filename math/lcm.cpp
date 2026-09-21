826ffa067dbf0e056687e5069cacfbd70862d34e
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-09-21 18:58:20 +0600

    till now

---
+++ math/lcm.cpp
@@ -0,0 +1,19 @@
+//! lcm = a*b / gcd(a,b)
+
+int gcd(int a, int b){
+    while(a>0 && b > 0){
+        if(a>b){
+            a = a % b;
+
+        } else {
+            b = b % a;
+        }
+    }
+    if(a==0) return b;
+    return a;
+}
+
+int lcm(int a, int b){
+    int gcd = gcd(a,b);
+    return (a*b)/gcd;
+}
