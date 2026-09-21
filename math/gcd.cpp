96ff8dcf68e0f0b404cb95b7814212fbaad1fc92
RAHIM KAWSAR <kahmedrahim1512@gmail.com>
2026-09-21 10:06:10 +0600

    gcd

--- math/gcd.cpp
+++ math/gcd.cpp
@@ -1,1 +1,16 @@
 //! gcd
+// gcd(a,b) = gcd(a%b,b) , a>b;
+//  gcd(a,b) = gcd(a,b%a) , b>a;
+
+int gcd(int a, int b){
+    while(a>0 && b > 0){
+        if(a>b){
+            a = a % b;
+
+        } else {
+            b = b % a;
+        }
+    }
+    if(a==0) return b;
+    return a;
+}
